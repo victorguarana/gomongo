08f32951561e5c834e54767f74b4cd463debd72b
Victor Guaraná <victorguarana@hotmail.com>
2023-03-04 01:21:52 -0300

    wip

---
+++ mongo/database.go
@@ -0,0 +1,17 @@
+package mongo
+
+// import (
+// 	"go.mongodb.org/mongo-driver/bson"
+// )
+
+// type DB interface {
+// 	First(string) (bson.M, error)
+// 	Create(string, bson.M) error
+// 	Delete(string, bson.M) error
+// 	All(string) (bson.A, error)
+// 	// TODO:
+// 	// Find()
+// 	// FindOrCreate()
+// 	// DeleteAll()
+// 	// Where()
+// }
