065adf33d3ceca7204d6ec0f31b7a8004347d56e
Victor Guaraná <victorguarana@hotmail.com>
2023-03-03 23:01:18 -0300

    refactor main to serve as an example

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
-package test
+package main
 
 import (
+	"fmt"
 	"gomongo/database/connection"
 	"gomongo/database/mongo"
 )
 
+var carroCollectionName = "carros"
+
+type Carro struct {
+	Marca  string
+	Modelo string
+	Ano    int
+}
+
 // Example of how to use
 func main() {
-	err := connection.Init("URI", "DatabaseName")
+	err := connection.Init("mongodb://localhost:27017", "Loja")
 	if err != nil {
 		panic(err)
 	}
 
-	create()
-}
+	// Example: Create document
+	carro := Carro{
+		Marca:  "Honda",
+		Modelo: "City",
+		Ano:    2022,
+	}
 
-func create() {
-	object := map[string]string{"Name": "Victor"}
+	err = mongo.Create(carroCollectionName, carro)
+	if err != nil {
+		panic(err)
+	}
 
-	mongo.Create("CollectionName", object)
+	// Example: List documents on Collection
+	listaCarros, err := mongo.All(carroCollectionName)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(listaCarros)
 }
