e146b6bdb916c64025ad53601e8ecacd09bf7bdd
Victor Guaraná <victorguarana@hotmail.com>
2023-03-29 01:10:29 -0300

    wip - add tests to collection

--- mongo/collection_test.go
+++ mongo/collection_test.go
@@ -1,13 +1,18 @@
 package mongo
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var _ = Describe("Collection", func() {
 	Describe("NewCollection", func() {
-		It("Returns correct collection instance", func() {
+		It("returns correct collection instance", func() {
 			expectedCollection := &collection{name: "Testing"}
 			receivedCollection := NewCollection("Testing")
 
@@ -16,7 +21,43 @@ var _ = Describe("Collection", func() {
 	})
 
 	Describe("init", func() {
-		// TODO
+		When("collection was not initialized", func() {
+			BeforeEach(func() {
+				client, _ := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+				mongoDatabase = client.Database("test_database")
+			})
+
+			AfterEach(func() {
+				mongoDatabase = nil
+			})
+			It("returns nil and set mongoCollection", func() {
+				c := collection{name: "test_init"}
+				err := c.init()
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(c.mongoCollection).NotTo(BeNil())
+			})
+		})
+
+		When("collection was already initialized", func() {
+			It("returns nil", func() {
+				initialMongoCollection := &mongo.Collection{}
+				c := collection{mongoCollection: initialMongoCollection}
+				err := c.init()
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(c.mongoCollection).To(Equal(initialMongoCollection))
+			})
+		})
+
+		When("mongoDatabase was not initialized", func() {
+			It("returns connection not inirialized error", func() {
+				c := collection{}
+				err := c.init()
+
+				Expect(err).To(MatchError(ErrConnectionNotInitialized))
+			})
+		})
 	})
 	Describe("All", func() {
 		// TODO
