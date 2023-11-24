4c0b6a29f8016c2a0596ba1a9f1398041dae79cb
Victor Guaraná <victor.corpas@g.globo>
2023-11-24 16:24:17 -0300

    add pingtimeout in connectionSettings

--- gomongo/connection.go
+++ gomongo/connection.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	ErrInvalidSettings      = errors.New("settings must be valid")
-	ErrGomongoCanNotConnect = errors.New("gomongo can not connect to mongo")
+	ErrGomongoCanNotConnect = errors.New("gomongo can not connect to mongodb")
 )
 
 var mongoDatabase *mongo.Database
@@ -21,6 +21,33 @@ type ConnectionSettings struct {
 	URI          string
 	DatabaseName string
 	Timeout      time.Duration
+	PingTimeout  time.Duration
+}
+
+func NewConnectionSettings() *ConnectionSettings {
+	return &ConnectionSettings{
+		PingTimeout: 10 * time.Second,
+	}
+}
+
+func (cs *ConnectionSettings) SetURI(uri string) *ConnectionSettings {
+	cs.URI = uri
+	return cs
+}
+
+func (cs *ConnectionSettings) SetDatabaseName(databaseName string) *ConnectionSettings {
+	cs.DatabaseName = databaseName
+	return cs
+}
+
+func (cs *ConnectionSettings) SetTimeout(timeout time.Duration) *ConnectionSettings {
+	cs.Timeout = timeout
+	return cs
+}
+
+func (cs *ConnectionSettings) SetPingTimeout(pingTimeout time.Duration) *ConnectionSettings {
+	cs.PingTimeout = pingTimeout
+	return cs
 }
 
 func (cs *ConnectionSettings) validate() error {
@@ -29,17 +56,17 @@ func (cs *ConnectionSettings) validate() error {
 	}
 
 	if cs.DatabaseName == "" {
-		return fmt.Errorf("%w: DatabaseName is invalid", ErrInvalidSettings)
+		return fmt.Errorf("%w: Database Name is invalid", ErrInvalidSettings)
 	}
 
-	if cs.Timeout <= 0 {
-		return fmt.Errorf("%w: Timeout is invalid", ErrInvalidSettings)
+	if cs.PingTimeout <= 0 {
+		return fmt.Errorf("%w: Ping Timeout is invalid", ErrInvalidSettings)
 	}
 
 	return nil
 }
 
-func Init(cs ConnectionSettings) error {
+func Init(cs *ConnectionSettings) error {
 	if err := cs.validate(); err != nil {
 		return err
 	}
@@ -53,7 +80,10 @@ func Init(cs ConnectionSettings) error {
 		return fmt.Errorf("%w: %w", ErrGomongoCanNotConnect, err)
 	}
 
-	if err := mongoClient.Ping(context.Background(), nil); err != nil {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), cs.PingTimeout)
+	defer cancelFunc()
+
+	if err := mongoClient.Ping(ctx, nil); err != nil {
 		return fmt.Errorf("%w: %w", ErrGomongoCanNotConnect, err)
 	}
 
